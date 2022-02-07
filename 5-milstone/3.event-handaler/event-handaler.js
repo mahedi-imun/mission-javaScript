5bf60b01201b23d9274d82d32c495c61a568e0e2
mahedi <mahedianik4@gmail.com>
2022-02-08 00:43:38 +0600

    event handeler

--- 5-milstone/3.event-handaler/event-handaler.js
+++ 5-milstone/3.event-handaler/event-handaler.js
@@ -8,4 +8,10 @@ function textElement (){
         p.innerText='iam using direct function'
 
     });
-    
+
+    document.getElementById('submit-btn').addEventListener('click',function(){
+        let inputText = document.getElementById('input-text');
+        let p=  document.getElementById('inner-text');
+        p.innerText = inputText.value;
+        inputText.value =" ";
+    });
