47ea799512957792622b4d8d9e22b87b9612e90f
mahedi <mahedianik4@gmail.com>
2022-03-14 01:13:55 +0600

    recap ready to react

---
+++ revition-for-react/app.js
@@ -0,0 +1,36 @@
+const dreamGirl = [
+    {
+      dream1: {
+        name: "bbu",
+        height: "5.4",
+        family: [{ father: "rock", mother: "shila", sister: "chinki" }],
+        age: undefined,
+        contactInfo: [
+          {
+            facebook: {
+              link: "https://www.facebook.com/",
+              followers: "12545",
+              status: "single",
+              friendsList: [
+                { name: "rofik" },
+                { name: "jobbar" },
+                { name: "salam" },
+                { name: "borkot" },
+                undefined,
+              ],
+            },
+          },
+          { instagram: "https://www.instagram.com/" },
+          { twitter: "https://twitter.com/" },
+          { github: "https://github.com/" },
+          { phone: ["01254823212", "02152457"] },
+        ],
+      },
+    },
+  ];
+
+  const friends = dreamGirl[0].dream1.contactInfo[0].facebook.friendsList;
+  for (let friend of friends || []){
+    console.log(friend?.name);
+  }
+ 
